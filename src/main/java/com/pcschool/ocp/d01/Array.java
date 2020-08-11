fe2e61001aea405d87d9a6f754e55eafa718a772
MB-study <MB-study@192.168.1.116>
2020-08-11 14:01:39 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/Array.java
@@ -0,0 +1,31 @@
+package com.pcschool.ocp.d01;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] scores = {100, 90, 80, 70};
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores[3]);
+        System.out.println(scores.length);
+        // 求總分 ?
+        int sum = 0;
+        for(int i=0;i<scores.length;i++){
+            sum += scores[i];
+        }
+        System.out.printf("總分: %d\n", sum);
+        // 求平均 ?
+        int avg = sum / scores.length;
+        System.out.printf("平均: %d\n", avg);
+        
+        double sum2= 0;
+        for(int i=0;i<scores.length;i++){
+            sum2+=Math.pow(scores[i]-avg, 2);
+        }
+        double sd = Math.sqrt((1.0/scores.length)*sum2);
+        System.out.printf("標準差: %.2f\n",sd);
+        //變異係數
+        double cv = sd/avg;
+        System.out.printf("變異係數: %.2f\n",cv);
+    }
+}
