453b605d95623b88b65e1585bfe4eb2e9aa125ef
MB-study <MB-study@192.168.1.116>
2020-08-13 10:40:20 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/case3/Poker.java
+++ src/main/java/com/pcschool/ocp/game/case3/Poker.java
@@ -1,27 +1,24 @@
-
 package com.pcschool.ocp.game.case3;
 
-
 public class Poker {
-    public String mark;//花色
-    public String name;//號碼
-    public String color;//顏色
-    public int value;//價值
-   //建構式
-    public Poker(String mark, String name, String color, int value) {
+    public String mark; // 花色
+    public String number; // 號碼
+    public String color; // 顏色
+    public int    value; // 價值
+    
+    // 建構式
+    public Poker(String mark, String number, String color, int value) {
         this.mark = mark;
-        this.name = name;
+        this.number = number;
         this.color = color;
         this.value = value;
     }
 
     @Override
     public String toString() {
-        return "Poker{" + "mark=" + mark + ", name=" + name + ", color=" + color + ", value=" + value + '}';
+        return mark + number;
     }
 
     
-    }
-       
-    
 
+}
