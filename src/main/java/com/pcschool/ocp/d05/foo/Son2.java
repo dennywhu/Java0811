9c5cc645700017d7874950e332c9a9fcdbfff6ec
MB-study <MB-study@192.168.1.116>
2020-08-14 14:21:13 +0800

    Java

--- src/main/java/com/pcschool/ocp/d05/foo/Son2.java
+++ src/main/java/com/pcschool/ocp/d05/foo/Son2.java
@@ -1,10 +1,9 @@
 package com.pcschool.ocp.d05.foo;
 
-import com.pcschool.ocp.d05.*;
+import com.pcschool.ocp.d05.Father;
 
-public class Son2  {
-    public void print(){
-        Father father = new Father();
-        System.out.println(father.money);
+public class Son2 extends Father {
+    public void print() {
+        System.out.println(money);
     }
 }
