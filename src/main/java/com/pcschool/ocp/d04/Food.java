53eb086742e4af26a170234950d41438db6ccd51
MB-study <MB-study@192.168.1.116>
2020-08-14 11:23:00 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d04/Food.java
@@ -0,0 +1,43 @@
+
+package com.pcschool.ocp.d04;
+
+import java.util.Random;
+
+
+public class Food {
+    public static String name;
+    private int amount=100;
+    private int price;
+    
+    static{
+        int n = new Random().nextInt(10);
+        if(n%2==0)
+        name = "可樂";
+        else
+        name = "糖水";    
+    }
+    //物件共同初始區段
+    {
+       int n = new Random().nextInt();
+        price = n%2==0 ? 100:50;
+        
+    }
+    
+    
+    public Food() {
+        
+    }
+
+    public Food(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" + "amount=" + amount + ", price=" + price + '}';
+    }
+}
+    
+    
+    
+    
