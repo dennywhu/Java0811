b9cbef5b103ae152d890b88e481afbfba2355099
MB-study <MB-study@192.168.1.116>
2020-08-14 10:09:32 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d04/Pen.java
@@ -0,0 +1,26 @@
+
+package com.pcschool.ocp.d04;
+
+
+public class Pen {
+    private  int price;
+
+    @Override
+    public String toString() {
+        return "Pen{" + "price=" + price + '}';
+    }
+
+    public Pen(int price) {
+        if(price <10)
+            this.price = 10;
+        else
+            this.price = price;
+    }
+
+    public Pen() {
+        this(5);
+    }
+    
+   
+    }
+
