1d0aeee03a72b1ff5bde268adf962c3374a849f5
MB-study <MB-study@192.168.1.116>
2020-08-17 13:49:25 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d06/case3/drink/MilkTea.java
@@ -0,0 +1,5 @@
+package com.pcschool.ocp.d06.case3.drink;
+
+public class MilkTea extends Drink {
+ 
+}
---
+++ src/main/java/com/pcschool/ocp/d06/case3/material/Milk.java
@@ -0,0 +1,5 @@
+package com.pcschool.ocp.d06.case3.material;
+
+public class Milk extends Material {
+ 
+}
