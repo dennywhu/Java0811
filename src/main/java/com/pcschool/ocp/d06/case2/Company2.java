1042732d1f303ca14ccb0d3ff006afb9de19460c
MB-study <MB-study@192.168.1.116>
2020-08-17 11:36:40 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case2/Company2.java
+++ src/main/java/com/pcschool/ocp/d06/case2/Company2.java
@@ -6,6 +6,7 @@ import com.pcschool.ocp.d06.case2.employee.Manager;
 import com.pcschool.ocp.d06.case2.employee.Operator;
 import com.pcschool.ocp.d06.case2.employee.Sales;
 import com.pcschool.ocp.d06.case2.employee.Supervistor;
+import java.util.Arrays;
 
 public class Company2 {
     public static void main(String[] args) {
@@ -33,6 +34,28 @@ public class Company2 {
             sum += emp.getSalary().getMoney();
         }
         System.out.printf("總薪資成本: %,d\n", sum);
-                
+         //sales總薪資成本=
+         sum = 0;{
+        for(Employee emp : employees) {
+            if (emp instanceof Sales){
+                 sum += emp.getSalary().getMoney();
+            }
+           
+        }
+        System.out.printf("SAlES薪資:%d\n",sum );
+    }
+         //Java 8
+         Arrays.stream(employees)
+                 .forEach(e -> System.out.println(e.getSalary().getMoney()));
+         
+         sum =Arrays.stream(employees).mapToInt(e -> e.getSalary().getMoney())
+                 .sum();
+         System.out.println(sum);//總薪資成本
+         //sales總薪資成本
+         sum = Arrays.stream(employees)
+                 .filter(e -> e instanceof Sales)
+                 .mapToInt(e -> e.getSalary().getMoney())
+                 .sum();
+         System.out.println(sum);//sales成本
     }
 }
