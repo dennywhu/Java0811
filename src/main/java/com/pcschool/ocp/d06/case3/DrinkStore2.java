9c4712a5784e0560a1d446c409e9251b2989f307
MB-study <MB-study@192.168.1.116>
2020-08-17 15:57:34 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/DrinkStore2.java
+++ src/main/java/com/pcschool/ocp/d06/case3/DrinkStore2.java
@@ -1,18 +1,21 @@
 package com.pcschool.ocp.d06.case3;
 
 import com.pcschool.ocp.d06.case3.drink.Coffee;
-import java.util.ArrayList;
-import  com.pcschool.ocp.d06.case3.drink.Drink;
-
+import com.pcschool.ocp.d06.case3.drink.Drink;
 import com.pcschool.ocp.d06.case3.drink.GreenPearlTea;
 import com.pcschool.ocp.d06.case3.drink.GreenTea;
 import com.pcschool.ocp.d06.case3.drink.MilkTea;
 import com.pcschool.ocp.d06.case3.drink.PearlTea;
+import com.pcschool.ocp.d06.case3.material.Milk;
+import com.pcschool.ocp.d06.case3.material.Pearl;
+import com.pcschool.ocp.d06.case3.material.Tea;
+import java.util.ArrayList;
+
 public class DrinkStore2 {
     public static void main(String[] args) {
-        //建立一個只能放Drink物件的動態陣列
+        // 建立一個只能放 Drink 物件的動態陣列
         ArrayList<Drink> drinks = new ArrayList<>();
-        drinks.add(new Coffee());//add()加入元素
+        drinks.add(new Coffee()); // add() 加入元素
         drinks.add(new GreenTea());
         drinks.add(new GreenTea());
         drinks.add(new GreenPearlTea());
@@ -28,11 +31,20 @@ public class DrinkStore2 {
         drinks.add(new PearlTea());
         drinks.add(new PearlTea());
         
-        //總共買了多少錢
+        // 1.總共買了多少錢 ?
         int sum = drinks.stream().mapToInt(d -> d.getPrice()).sum();
-        System.out.printf("總共買了多少錢:$%,d\n",sum);
-        //成分中有奶製品的共有多少錢？
+        System.out.printf("總共買了多少錢: $%,d\n", sum);
         
-                
+        // 2.成分中有奶製品(Milk)的共有多少錢 ?
+        sum = drinks.stream()
+                .filter(d -> d instanceof MilkTea || d instanceof Coffee || d instanceof PearlTea || d instanceof GreenPearlTea )
+                .mapToInt(d -> d.getPrice()).sum();
+        System.out.printf("有奶製品(Milk)總共買了多少錢: $%,d\n", sum);        
+        
+        // 改良
+        sum = drinks.stream()
+                .filter(d -> d.getMaterials().stream().filter(m -> m instanceof Milk).findAny().isPresent())
+                .mapToInt(d -> d.getPrice()).sum();
+        System.out.printf("有奶製品(Milk)總共買了多少錢: $%,d\n", sum);        
     }
 }
