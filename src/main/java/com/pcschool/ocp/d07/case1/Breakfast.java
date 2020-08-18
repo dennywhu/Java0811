a176e3ef1f7c68228671491c3ca38ef36e999944
MB-study <MB-study@192.168.1.116>
2020-08-18 11:38:42 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case1/Breakfast.java
+++ src/main/java/com/pcschool/ocp/d07/case1/Breakfast.java
@@ -3,11 +3,14 @@ package com.pcschool.ocp.d07.case1;
 
 import com.pcschool.ocp.d07.case1.food.Hamburger;
 import com.pcschool.ocp.d07.case1.food.Sandwich;
+import com.pcschool.ocp.d07.case1.material.Hotdog;
 
 
 public class Breakfast {
     public static void main(String[] args) {
         Sandwich sandwich = new Sandwich("火腿蛋");
+        Hotdog hotdog = new Hotdog();
+        sandwich.addMaterial(hotdog);
         System.out.println(sandwich.getName());
         System.out.println(sandwich.getPrice());//25
         
