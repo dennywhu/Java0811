4714df58872519c2e8c80e505925fe010499dfbc
MB-study <MB-study@192.168.1.116>
2020-08-24 15:54:40 +0800

    Java

--- src/main/java/com/pcschool/ocp/d12_exception/case5/Client.java
+++ src/main/java/com/pcschool/ocp/d12_exception/case5/Client.java
@@ -1,14 +1,12 @@
 package com.pcschool.ocp.d12_exception.case5;
 
-
-
 public class Client {
     public static void main(String[] args) {
         accessLogin();
     }
     
-    public static void accessLogin(){
-        LoginService service = new LoginService();
+    public static void accessLogin() {
+        LoginService service = LoginService.getInstance();
         String username = "john";
         String password = "1234";
         boolean check;
@@ -16,7 +14,7 @@ public class Client {
             check = service.login(username, password);
             System.out.println(check);
         } catch (LoginException ex) {
-            System.out.println(ex);
+            System.out.println(ex.getMessage());
             ex.how2do();
         }
         
