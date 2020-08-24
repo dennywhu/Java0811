b8e07cc43aabdf0cb4aea84b2835264d3678500c
MB-study <MB-study@192.168.1.116>
2020-08-24 11:46:34 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d11/case2/ItemGroupDemo.java
@@ -0,0 +1,34 @@
+package com.pcschool.ocp.d11.case2;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.Function;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toSet;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemGroupDemo {
+
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 20, new BigDecimal("19.99")),
+                new Item("orang", 10, new BigDecimal("29.99")),
+                new Item("watermelon", 10, new BigDecimal("29.99")),
+                new Item("papaya", 20, new BigDecimal("9.99")),
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 10, new BigDecimal("19.99")),
+                new Item("apple", 20, new BigDecimal("9.99"))
+        );
+        //每一種水果各有幾箱
+        Map<String, Long> group1 = items.stream()
+                .collect(groupingBy(Item::getName, counting()));
+        System.out.println(group1);
+    }
+}
