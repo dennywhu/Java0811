a0bb77e006b0a46330d1e0d2fead1304c7e185e3
MB-study <MB-study@192.168.1.116>
2020-08-27 10:03:02 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d17_io/case2/RecScanPath.java
@@ -0,0 +1,32 @@
+package com.pcschool.ocp.d17_io.case2;
+
+import java.io.File;
+
+public class RecScanPath {
+    static int java_file_count = 0;
+    public static void main(String[] args) {
+        String s = "src\\main\\java\\com\\pcschool\\ocp";
+        File path = new File(s);
+        scan(path);
+        System.out.printf("java_file_count: %d 支\n", java_file_count);
+    }
+
+    public static void scan(File path) {
+        try {
+            for (String fname : path.list()) {
+                File f = new File(path.getPath() + File.separator + fname);
+                System.out.printf("%s 是%s\n", f.getPath(), f.isFile() ? "檔案" : "目錄");
+                if (f.isDirectory()) {
+                    scan(f);
+                } else {
+                    if(f.getName().contains(".java")) {
+                        ++java_file_count;
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+
+    }
+
+}
--- src/main/java/com/pcschool/ocp/d17_io/case2/ScanPath.java
+++ src/main/java/com/pcschool/ocp/d17_io/case2/ScanPath.java
@@ -5,9 +5,9 @@ import java.io.File;
 public class ScanPath {
     public static void main(String[] args) {
         File path = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io");
-    for(String fname : path.list()){
-        File f = new File(path.getPath() + "/" + fname);
-        System.out.printf("%s 是 %s 他是%s\n",fname,path.getPath(),f.isDirectory()?"目錄" : "檔案");
-    }
+        for(String fname : path.list()) {
+            File f = new File(path.getPath() + "/" + fname); // 組合新的路徑
+            System.out.printf("%s 的完整路徑是: %s 它是: %s\n", fname, f.getPath(), f.isDirectory()?"目錄":"檔案");
+        }
     }
 }
