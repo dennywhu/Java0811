d4f0f98698215c12a3c7d8c4f0fc818387604c0f
MB-study <MB-study@192.168.1.116>
2020-08-27 09:42:43 +0800

    Java

--- src/main/java/com/pcschool/ocp/d17_io/case2/ScanPath.java
+++ src/main/java/com/pcschool/ocp/d17_io/case2/ScanPath.java
@@ -5,8 +5,9 @@ import java.io.File;
 public class ScanPath {
     public static void main(String[] args) {
         File path = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io");
-    for(String f name : path.list()){
-        System.out.println(fname);
+    for(String fname : path.list()){
+        File f = new File(path.getPath() + "/" + fname);
+        System.out.printf("%s 是 %s 他是%s\n",fname,path.getPath(),f.isDirectory()?"目錄" : "檔案");
     }
     }
 }
